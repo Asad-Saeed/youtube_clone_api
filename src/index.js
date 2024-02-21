7a86fde3fdc89b6ce750f330e57c58b925ec9e52
Asad Saeed <asadsaeed12225@gmail.com>
2024-02-21 21:07:25 +0500

    feat:connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,22 +1,4 @@
-import mongoose from "mongoose";
-import express from "express";
-
-const app = express();
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERROR:", error);
-      throw error;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`App is running on ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR:", error);
-    throw error;
-  }
-};
-
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({ path: "./env" });
 connectDB();
