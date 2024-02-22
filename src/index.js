ddc7bce5a569d7861dffbee7f99af09c50dc7add
Asad Saeed <asadsaeed12225@gmail.com>
2024-02-22 19:55:19 +0500

    feat:basic configration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
-connectDB();
+const port = process.env.PORT;
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Application is running on port:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB connection error:", error);
+  });
