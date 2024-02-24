bfea5c2a10481b981228858c8c6fc128cab813d6
Asad Saeed <asadsaeed12225@gmail.com>
2024-02-24 19:29:21 +0500

    feat:multer file upload configration

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    // filename handle here
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+    // cb(null, file.originalname);  // used for user defult name
+  },
+});
+
+export const upload = multer({ storage: storage });
