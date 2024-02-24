cf225ce5cee97bee769caf631ecd44953c2e1cc8
Asad Saeed <asadsaeed12225@gmail.com>
2024-02-24 19:17:17 +0500

    feat:cloudinary file upload configration

---
+++ src/utils/cloudinary/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "node:fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Upload on cloudinary
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //   upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file upladed successfully
+    console.log("file is uploaded successfully on cloudinary", response?.url);
+    return response;
+  } catch (error) {
+    // remove the locally saved file as the upload operation got failed
+    fs.unlinkSync(localFilePath);
+    console.log("Error in file uplaoding", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
