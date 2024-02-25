cbce3c8575d36d9d0b83660588357c230b2752fa
Asad Saeed <asadsaeed12225@gmail.com>
2024-02-25 11:47:50 +0500

    feat:router and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,11 @@
 import { asyncHandler } from "../utils/handler/async.handler.js";
 
-const registerUser = asyncHandler(async (req, res) => {
+const registerUser = asyncHandler(async (req, res, next) => {
+  console.log("registerUser route handler called");
   res.status(200).json({ message: "ok" });
 });
 
-const loginUser = asyncHandler(async (req, res) => {
+const loginUser = asyncHandler(async (req, res, next) => {
   res.status(200).json({ message: "ok" });
 });
 
--- src/utils/handler/async.handler.js
+++ src/utils/handler/async.handler.js
@@ -1,22 +1,24 @@
 // Promises
-const asyncHandler = (requestHandler) => {
- return (error, req, res, next) => {
-    Promise.resolve(requestHandler(error, req, res, next)).catch((error) =>
-      next()
-    );
-  };
-};
+// const asyncHandler = (requestHandler) => {
+//  return ( req, res, next) => {
+//     Promise.resolve(requestHandler( req, res, next)).catch((error) =>
+//       next()
+//     );
+//   };
+// };
 
 // Try catch
-// const asyncHandler = (requestHandler) => async (error, req, res, next) => {
-//     try {
-//         await requestHandler(error, req, res, next);
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success: false,
-//             message: error.message,
-//         });
-//     }
-// };
+
+const asyncHandler = (requestHandler) => async (req, res, next) => {
+  try {
+    await requestHandler(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message,
+    });
+    next(error);
+  }
+};
 
 export { asyncHandler };
